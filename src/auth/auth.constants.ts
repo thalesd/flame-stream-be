a31d8c8bdb365716e3aa433418774a5ebb0b01d2
thalesd <thales.doni.ol@gmail.com>
2024-04-13 20:18:39 -0300

    Increases JWT expiration time to a week

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,4 +1,5 @@
 export const jwtConstants = {
   secret:
     'md873hgbqbgv89fgb84bgrf8bgrb8iycvdytvcydgv8dvc8db82743e97y5923hbg8fbvs7vvbsfbv7sf',
+  expiresIn: '7 days',
 };
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { jwtConstants } from './auth.constants';
 
 @Injectable()
 export class AuthService {
@@ -23,7 +24,9 @@ export class AuthService {
   async loginUser(user: any) {
     const payload = { username: user.username, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, {
+        expiresIn: jwtConstants.expiresIn
+      }),
     };
   }
 }
